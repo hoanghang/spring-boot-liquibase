43236d7ce92c74c5b8e4ec10fd26e39b849e1c67
hoanghang <hoanghangnd2810@gmail.com>
2018-06-19 15:00:05 +0700

    class Role

---
+++ qltd/src/main/java/com/qltd/entities/Role.java
@@ -0,0 +1,29 @@
+package com.qltd.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Role {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private long id;
+  private String name;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
