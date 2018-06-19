5c7a3d8b51802be2cb124949afcda1c3b7d2a85d
hoanghang <hoanghangnd2810@gmail.com>
2018-06-19 15:59:17 +0700

    feature delete customer

--- qltd/src/main/java/com/qltd/controller/CustomerController.java
+++ qltd/src/main/java/com/qltd/controller/CustomerController.java
@@ -69,6 +69,12 @@ public class CustomerController {
     public String login(@RequestParam(value="name", defaultValue="World") String name) {
         return "login _ hotfix.";
     }
+
+    @RequestMapping("/login")
+    @ResponseBody
+    public String delete(@RequestParam(value="name", defaultValue="World") String name) {
+        return "delete customer.";
+    }
 }
 
 
